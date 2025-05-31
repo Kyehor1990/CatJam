59183ce0ebb9593a152ae960a430d3e4b7f59a0b
Kyehor1990 <ardauludag2005@gmail.com>
2025-05-31 13:33:24 +0300

    .

--- Assets/CatJam/Scripts/Player/PlayerHealth.cs
+++ Assets/CatJam/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 5;
     private int currentHealth;
+
+    [SerializeField] Slider healthSlider;
 
     private PlayerMovement playerMovement;
 
@@ -11,11 +14,17 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth = maxHealth;
         playerMovement = GetComponent<PlayerMovement>();
+
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
     }
 
     public void TakeDamage(int amount)
     {
-        if (playerMovement != null && playerMovement.IsDodging())
+        /*if (playerMovement != null && playerMovement.IsDodging())
         {
             Debug.Log("Dodge aktif! Hasar alınmadı.");
             return;
@@ -27,6 +36,23 @@ public class PlayerHealth : MonoBehaviour
         if (currentHealth <= 0)
         {
             Die();
+        }*/
+
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
         }
     }
 
